f42308b1a3da996b90256bccb3abb1540349dda3
wutuobangduan <wutuobangduan@163.com>
2015-06-02 14:50:44 +0800

    modify settings.py with scrapy-redis

--- tc58/settings.py
+++ tc58/settings.py
@@ -17,7 +17,7 @@ BOT_NAME = 'tc58'
 
 SPIDER_MODULES = ['tc58.spiders']
 NEWSPIDER_MODULE = 'tc58.spiders'
-ITEM_PIPELINES = ['tc58.pipelines.Tc58Pipeline']
+# ITEM_PIPELINES = ['tc58.pipelines.Tc58Pipeline']
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tc58 (+http://www.yourdomain.com)'
 
@@ -33,7 +33,43 @@ RETRY_ENABLED = False
 REDIRECT_ENABLED = False
 COOKIES_ENABLED = False
 DOWNLOAD_TIMEOUT = 15
-DOWNLOAD_DELAY = 0.5
+DOWNLOAD_DELAY = 1
+
+
+# Enables scheduling storing requests queue in redis.
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+
+# Don't cleanup redis queues, allows to pause/resume crawls.
+SCHEDULER_PERSIST = True
+
+# Schedule requests using a priority queue. (default)
+SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.SpiderPriorityQueue'
+
+# Schedule requests using a queue (FIFO).
+#SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.SpiderQueue'
+
+# Schedule requests using a stack (LIFO).
+# SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.SpiderStack'
+
+# Max idle time to prevent the spider from being closed when distributed crawling.
+# This only works if queue class is SpiderQueue or SpiderStack,
+# and may also block the same time when your spider start at the first time (because the queue is empty).
+# SCHEDULER_IDLE_BEFORE_CLOSE = 10
+
+# Store scraped item in redis for post-processing.
+ITEM_PIPELINES = [
+    'scrapy_redis.pipelines.RedisPipeline',
+    'tc58.pipelines.Tc58Pipeline',
+]
+
+# Specify the host and port to use when connecting to Redis (optional).
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+# Specify the full Redis URL for connecting (optional).
+# If set, this takes precedence over the REDIS_HOST and REDIS_PORT settings.
+# REDIS_URL = 'redis://user:pass@hostname:9001'
+
 
 PROXIES = [
     {'ip_port': 'xx.xx.xx.xx:xxxx', 'user_pass': 'foo:bar'},
